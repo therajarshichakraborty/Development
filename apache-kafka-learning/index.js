11b3185aeb493dbe49016c3d7e93910bf60d80f2
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-08-07 00:14:50 +0530

    feat:add learning apache-kafka setup

---
+++ apache-kafka-learning/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
